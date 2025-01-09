adf13592282c039006234593d9a8dca265cf1e56
Sagar Raj <sgrraj96@gmail.com>
2025-01-09 15:23:19 +0530

    private implemented

--- OOPS.cpp
+++ OOPS.cpp
@@ -1,9 +1,29 @@
 #include<iostream>
 using namespace std;
 class teacher{
+    private:
+    double salary;
+
     public:
-      
+      string name;
+      string subject;
+
+       void setSalary(double s){
+          salary=s;
+      }
+
+      double getSalary(){
+        return salary;
+      }
+    
+
 };
 int main(){
+    teacher t1;
+    t1.name="Sagar Raj";
+    t1.subject="c++";
+    t1.setSalary(25000);
+    cout<<t1.name<<endl;
+    cout<<t1.getSalary()<<endl;
 
 }
