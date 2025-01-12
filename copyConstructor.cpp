ed7337f3ce3c296f8c02e24cbd0d006d191e38de
Sagar Raj <sgrraj96@gmail.com>
2025-01-12 19:15:42 +0530

    copy constructor

--- copyConstructor.cpp
+++ copyConstructor.cpp
@@ -6,11 +6,40 @@ class Teacher{
     string subj;
     int rollno;
     string dept;
+   
+    Teacher(string name,string subj,int rollno,string dept){
+      this->name=name;
+      this->subj=subj;
+      this->rollno=rollno;
+      this->dept=dept;
+      
+    }
+          // self made copy constructor 
+    Teacher (Teacher &orgObj){
+      this->name=orgObj.name;
+      this->subj=orgObj.subj;
+      this->rollno=orgObj.rollno;
+      this->dept=orgObj.dept;
+    }
 
+    void getInfo(){
+      cout<<name<<endl;
+      cout<<subj<<endl;
+      cout<<rollno<<endl;
+      cout<<dept<<endl;
+    }
 
 };
 
 int main(){
-  Teacher t1
+  Teacher t1("sagar","c++",2121442,"cse");
+  // t1.getInfo();
+  // cout<<" "<<endl;
+  
+
+  Teacher t2(t1);
+  cout<<"i am 2nd obj"<<endl;
+    t2.getInfo();
+
 
 }
