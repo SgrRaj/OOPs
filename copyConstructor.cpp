254b957ad41b60d528f40f74f1fd71b01f99d96a
Sagar Raj <sgrraj96@gmail.com>
2025-01-12 15:25:51 +0530

    copy constructor

---
+++ copyConstructor.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+class Teacher{
+  public:
+    string name;
+    string subj;
+    int rollno;
+    string dept;
+
+
+};
+
+int main(){
+  Teacher t1
+
+}
--- thisPointer.cpp
+++ thisPointer.cpp
@@ -9,10 +9,31 @@ class Teacher{
       int rollno;
       string dept;
 
-      Teacher(){
+      Teacher(string name,string subj,int rollno,string dept){
+        this->name=name;
+        this->subj=subj;
+        this->rollno=rollno;
+        this->dept=dept;
         
       }
+
+      void setSalary(double s){
+        salary=s;
+      }
+      double getSalary(){
+        return salary;
+
+      }
+      
+      void getInfo(){
+        cout<<name<<endl;
+        cout<<subj<<endl;
+        cout<<rollno<<endl;
+        cout<<dept<<endl;
+      }
+
 };
 int main(){
-
+    Teacher t1("sagar","c++",2121442,"cse");
+      t1.getInfo();
 }
